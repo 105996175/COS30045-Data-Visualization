bc5367e57016922ba57eceeff57c0ff50b7b57ab
105996175 <105996175@student.swin.edu.au>
2026-09-13 13:00:25 +0800

    Add responsive D3 SVG canvas and test rectangle

--- Exercise 4/Exercise 4.3/assets/js/main.js
+++ Exercise 4/Exercise 4.3/assets/js/main.js
@@ -1,14 +1,12 @@
-d3.select("#d3-heading")
-    .style("color", "green");
+const svg = d3.select(".responsive-svg-container")
+    .append("svg")
+    .attr("viewBox", "0 0 1200 1600")
+    .style("border", "1px solid black");
 
-d3.select(".d3-container")
-    .append("p")
-    .text("Purchasing a low energy consumption TV will help with your energy bills!");
-
-d3.select(".d3-svg")
+svg
     .append("rect")
-    .attr("x", 50)
-    .attr("y", 50)
-    .attr("width", 100)
-    .attr("height", 30)
-    .style("fill", "green");
+    .attr("x", 10)
+    .attr("y", 10)
+    .attr("width", 414)
+    .attr("height", 16)
+    .attr("fill", "blue");
