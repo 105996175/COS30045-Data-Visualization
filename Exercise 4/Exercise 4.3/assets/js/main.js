95d96f1dcc7419eb74d438cd0afd4c8dac07cb04
105996175 <105996175@student.swin.edu.au>
2026-09-13 12:22:35 +0800

    Use D3 to style and append HTML elements

---
+++ Exercise 4/Exercise 4.3/assets/js/main.js
@@ -0,0 +1,6 @@
+d3.select("#d3-heading")
+    .style("color", "green");
+
+d3.select(".d3-container")
+    .append("p")
+    .text("Purchasing a low energy consumption TV will help with your energy bills!");
