616f7872115f277df17a94a840f49c83140ddd6f
105996175 <105996175@student.swin.edu.au>
2026-09-13 12:52:13 +0800

    Add D3 styling, paragraph and SVG rectangle for Exercise 4.2

--- Exercise 4/Exercise 4.3/assets/js/main.js
+++ Exercise 4/Exercise 4.3/assets/js/main.js
@@ -4,3 +4,11 @@ d3.select("#d3-heading")
 d3.select(".d3-container")
     .append("p")
     .text("Purchasing a low energy consumption TV will help with your energy bills!");
+
+d3.select(".d3-svg")
+    .append("rect")
+    .attr("x", 50)
+    .attr("y", 50)
+    .attr("width", 100)
+    .attr("height", 30)
+    .style("fill", "green");
