23a0e67c7c790621cd2e71a0158014a0f36bcbf1
105996175 <105996175@student.swin.edu.au>
2026-09-13 15:06:53 +0800

    Add brand and count labels to D3 bar chart for Exercise 4.7

--- Exercise 4/Exercise 4.7/assets/js/main.js
+++ Exercise 4/Exercise 4.7/assets/js/main.js
@@ -8,26 +8,41 @@ const drawBarChart = data => {
 
     const xScale = d3.scaleLinear()
         .domain([0, 1100])
-        .range([0, 500]);
+        .range([0, 350]);
 
     const yScale = d3.scaleBand()
         .domain(data.map(d => d.brand))
         .range([0, 500])
         .padding(0.1);
 
-    svg
-        .selectAll("rect")
+    const barAndLabel = svg
+        .selectAll("g")
         .data(data)
-        .join("rect")
-        .attr("class", d => {
-            console.log(d);
-            return `bar bar-${d.count}`;
-        })
-        .attr("x", 0)
-        .attr("y", d => yScale(d.brand))
+        .join("g")
+        .attr("transform", d => `translate(0, ${yScale(d.brand)})`);
+
+    barAndLabel
         .attr("width", d => xScale(d.count))
         .attr("height", yScale.bandwidth())
-        .attr("fill", "blue");
+        .attr("fill", "blue")
+        .attr("x", 100)
+        .attr("y", 0);
+
+
+    barAndLabel
+        .append("text")
+        .text(d => d.brand)
+        .attr("x", 90)
+        .attr("y", 15)
+        .attr("text-anchor", "end")
+        .style("font-size", "13px");
+
+    barAndLabel
+        .append("text")
+        .text(d => d.count)
+        .attr("x", d => 100 + xScale(d.count) + 4)
+        .attr("y", 12)
+        .style("font-size", "13px");
 };
 
 
